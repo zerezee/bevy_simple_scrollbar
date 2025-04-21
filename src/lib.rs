775d7bf3e0f7f10ef78dab7083836ee43a335d72
Zereze <zereze2008@gmail.com>
2025-04-21 17:31:57 +0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,29 @@
-#![doc = include_str!("../README.md")]
+/*!
+
+
+# Bevy Simple Scrollbar
+
+
+A simple crate to make it easier to make scrollbars, until bevy provides its own implementation for scrollbars.
+
+In action: 
+ 
+![Vertical Scrollbar](../vertical.gif)
+![Horizontal Scrollbar](../horizontal.gif)
+
+## Installation
+
+```sh
+cargo add bevy_simple_scrollbar
+```
+
+## Bevy Compatibility Table
+
+Bevy Version | Crate Version
+--- | ---
+0.15 | 0.1
+
+*/
 
 pub mod prelude {
     use bevy::prelude::*;
